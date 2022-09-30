111bd470789f12dc85a279c029da40d64e2849ba
dolnamool564 <dolnamool564@mumun.co.kr>
2022-09-30 15:19:29 +0900

    firstInit

---
+++ app/src/main/java/com/example/mytracker/ui/main/MainFragment.kt
@@ -0,0 +1,43 @@
+package com.example.mytracker.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import com.example.mytracker.R
+import org.w3c.dom.Text
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    @Composable
+    fun messageCard(name: String) {
+
+    }
+
+    @Composable
+    fun PreviewMessageCare() {
+
+    }
+}
